7012c185a42bf13af8427bcc901461e32eb0c96b
jbuusao <jbuusao@gmail.com>
2014-02-04 22:29:38 +0100

    Added test of the HMC6352 compass module

---
+++ HMC6352 Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+using astra;
+
+namespace Test_HMC6352
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            HMC6352 compass = new HMC6352(0x21);
+
+            while (true)
+            {
+                Thread.Sleep(1);
+                int heading = compass.getHeading();
+            }
+        }
+    }
+}
---
+++ HMC6352 Test/README.md
@@ -0,0 +1,4 @@
+A testbench of the HMC6352 compass module(https://www.sparkfun.com/products/7915), using a FEZ Domino (also tested OK against a Netduino)
+
+Usage: just gently move the board; compass data is sent to the serial output port of the FEZ / Netduino board
+
